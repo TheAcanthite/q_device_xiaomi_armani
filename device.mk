66b13f09bde90fc81c535ec508c54472e5da1133
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-12 15:48:17 -0700

    armani: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- device.mk
+++ device.mk
@@ -22,8 +22,10 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xhdpi 280dpi hdpi tvdpi mdpi ldpi
 
+# Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+TARGET_BOOTANIMATION_HALF_RES := true
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
