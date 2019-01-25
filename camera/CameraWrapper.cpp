74c4bfa2444cf81f8ebae71257ef5c70f100eaf4
luk1337 <priv.luk@gmail.com>
2019-01-25 17:01:45 +0200

    armani: Move custom camera parameters to CameraWrapper

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016, The CyanogenMod Project
+ * Copyright (C) 2019, The Lineage Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,8 @@
 #define OPEN_RETRIES    10
 #define OPEN_RETRY_MSEC 40
 
+using namespace android;
+
 static android::Mutex gCameraWrapperLock;
 static camera_module_t *gVendorModule = 0;
 
@@ -45,6 +47,13 @@ static int camera_device_open(const hw_module_t *module, const char *name,
         hw_device_t **device);
 static int camera_get_number_of_cameras(void);
 static int camera_get_camera_info(int camera_id, struct camera_info *info);
+
+const char KEY_QC_AE_BRACKET_HDR[] = "ae-bracket-hdr";
+const char KEY_QC_CAPTURE_BURST_EXPOSURE[] = "capture-burst-exposures";
+const char KEY_QC_MORPHO_HDR[] = "morpho-hdr";
+const char KEY_QC_ZSL[] = "zsl";
+const char FOCUS_MODE_MANUAL_POSITION[] = "manual";
+const char WHITE_BALANCE_MANUAL_CCT[] = "manual-cct";
 
 static struct hw_module_methods_t camera_module_methods = {
     .open = camera_device_open
@@ -112,7 +121,7 @@ static char *camera_fixup_getparams(int id, const char *settings)
 
     /* Remove HDR mode in front camera */
     if (id == 1) {
-        params.set(android::CameraParameters::KEY_SUPPORTED_SCENE_MODES,
+        params.set(KEY_SUPPORTED_SCENE_MODES,
             "auto,asd,landscape,snow,beach,sunset,night,portrait,backlight,sports,steadyphoto,flowers,candlelight,fireworks,party,night-portrait,theatre,action,AR");
     }
 
@@ -150,24 +159,24 @@ static char *camera_fixup_setparams(int id, const char *settings)
 
     /* Disable ZSL and HDR snapshots in video mode */
     if (videoMode) {
-        params.set(android::CameraParameters::KEY_QC_ZSL, "off");
+        params.set(KEY_QC_ZSL, "off");
         if (hdrMode) {
             params.set(android::CameraParameters::KEY_SCENE_MODE, "auto");
         }
     } else {
-        params.set(android::CameraParameters::KEY_QC_ZSL, "on");
+        params.set(KEY_QC_ZSL, "on");
     }
 
     /* Enable Morpho EasyHDR and disable flash in HDR mode */
     if (hdrMode && !videoMode) {
-        params.set(android::CameraParameters::KEY_QC_MORPHO_HDR, "true");
-        params.set(android::CameraParameters::KEY_QC_AE_BRACKET_HDR, "AE-Bracket");
-        params.set(android::CameraParameters::KEY_QC_CAPTURE_BURST_EXPOSURE, "-6,8,0");
+        params.set(KEY_QC_MORPHO_HDR, "true");
+        params.set(KEY_QC_AE_BRACKET_HDR, "AE-Bracket");
+        params.set(KEY_QC_CAPTURE_BURST_EXPOSURE, "-6,8,0");
         params.set(android::CameraParameters::KEY_FLASH_MODE, android::CameraParameters::FLASH_MODE_OFF);
     } else {
-        params.set(android::CameraParameters::KEY_QC_MORPHO_HDR, "false");
-        params.set(android::CameraParameters::KEY_QC_AE_BRACKET_HDR, "Off");
-        params.set(android::CameraParameters::KEY_QC_CAPTURE_BURST_EXPOSURE, "0,0,0");
+        params.set(KEY_QC_MORPHO_HDR, "false");
+        params.set(KEY_QC_AE_BRACKET_HDR, "Off");
+        params.set(KEY_QC_CAPTURE_BURST_EXPOSURE, "0,0,0");
     }
 
 #if !LOG_NDEBUG
