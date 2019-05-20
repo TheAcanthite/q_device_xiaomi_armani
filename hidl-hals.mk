c31be9da753ac4dd7daee004deb4ac675301ad37
haran <harangovindan@gmail.com>
2019-05-20 15:48:58 +0200

    armani: Remove Livedisplay support
    * Seems like livedisplay is not supported on armani

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -69,10 +69,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
-	
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Power
 PRODUCT_PACKAGES += \
