70a81c211ca2426be5a4cc20a373c7cb7f132a20
haran <harangovindan@gmail.com>
2019-06-02 17:13:38 +0200

    armani: Avoid enforcing RRO targets

--- device.mk
+++ device.mk
@@ -169,9 +169,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
 	
 # Perf
 PRODUCT_PACKAGES += \
