7b21e8cdc889f711525e736df1cacd85e44d9eef
haran <harangovindan@gmail.com>
2019-08-11 18:23:11 +0200

    armani: Build camera.device@1.0-impl

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -28,7 +28,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
 
 # Configstore
 PRODUCT_PACKAGES += \
